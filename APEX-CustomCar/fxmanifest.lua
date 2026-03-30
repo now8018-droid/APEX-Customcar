f9535843ada6e4cde8ddf55218c44468a3ed88c8
now8018-droid <now8018@gmail.com>
2026-03-31 00:46:57 +0700

    Fix fxmanifest stream file paths for gen9 color data

--- APEX-CustomCar/fxmanifest.lua
+++ APEX-CustomCar/fxmanifest.lua
@@ -18,9 +18,9 @@ files {
 	'ui/font/**/*.ttf',
 	'ui/img/**/*.png',
 	'ui/sounds/**/*.ogg',
-	"data/carcols_gen9.meta",
-	"data/carmodcols_gen9.meta",
-	"data/carmodcols.ymt",
+	"stream/carcols_gen9.meta",
+	"stream/carmodcols_gen9.meta",
+	"stream/carmodcols.ymt",
 	"stream/vehicle_paint_ramps.ytd"
 }
 
